d28a2d073f1f02249923a4ca3c3ea8f6c8ad47e2
Kristie Poon <itlm071@scc2101m-21.ucdavis.edu>
2024-02-23 17:18:13 -0800

    every picture

--- studio2/script.js
+++ studio2/script.js
@@ -1,144 +1,61 @@
 (function () {
     'use strict';
 
-    // Adding Scrolling Captions
-    const captions = [
-        '',            // Empty string for the first element
-        '1. yeosangs',
-        '2. hongjoong',
-        '3. seonghwa',
-        '4. yunho',
-        '5. san',
-        '6. mingi',
-        '7. wooyoung',
-        '8. jongho'
-    ];
-
-    let figCaption = document.querySelector('figcaption');
     let img = document.querySelector('figure img');
     let defaultStyle = img.style.cssText;
+    let isHovered = false;
 
-    figCaption.innerHTML = captions[1];
+    document.querySelectorAll('.polaroid').forEach(function (polaroid, index) {
+        polaroid.addEventListener('mouseover', function () {
+            isHovered = true;
+            moveImageToSection(index + 1); // Adjust to match section indices
+        });
 
-    window.onbeforeunload = function () {
-        window.scrollTo(0, 0);
+        polaroid.addEventListener('mouseout', function () {
+            isHovered = false;
+            resetImage();
+        });
+    });
+
+    function moveImageToSection(sectionIndex) {
+        const translation = getTranslationForSection(sectionIndex);
+        
+        img.style.transition = 'transform 0.8s ease-in-out'; // Adjust the easing function
+        img.style.transform = `translate(${translation.left}px, ${translation.top}px) scale(${translation.scale})`;
     }
 
-    window.addEventListener('load', function () {
-        const posts = document.querySelectorAll('section');
-        let postTops = [];
-        let pageTop;
-        let counter = 1;
-        let prevCounter = 1;
-        let doneResizing;
-        let exitDirection;
-        let enterDirection;
-
-        resetPagePosition();
-
-        window.addEventListener('scroll', function () {
-            pageTop = window.pageYOffset + 300;
-
-            // Check scroll direction
-            if (pageTop > postTops[counter]) {
-                counter++;
-                console.log(`scrolling down ${counter}`);
-            } else if (counter > 1 && pageTop < postTops[counter - 1]) {
-                counter--;
-                console.log(`scrolling up ${counter}`);
-            }
-
-            if (counter != prevCounter) {
-                img.className = 'sect' + counter;
-                if (counter > prevCounter) {
-                    exitDirection = 'animate exitup';
-                    enterDirection = 'animate enterup';
-                } else {
-                    exitDirection = 'animate exitdown';
-                    enterDirection = 'animate enterdown';
-                }
-
-                // Animation is completed
-                figCaption.className = exitDirection;
-                figCaption.addEventListener('animationend', function () {
-                    let newCaption = document.querySelector('figcaption').cloneNode(true);
-                    figCaption.remove();
-                    newCaption.className = enterDirection;
-                    newCaption.innerHTML = captions[counter];
-                    document.querySelector('figure').appendChild(newCaption);
-                    figCaption = document.querySelector('figcaption');
-                });
-
-                prevCounter = counter;
-            }
-        }); // end window scroll function
-
-        window.addEventListener('resize', function () {
-            clearTimeout(doneResizing);
-
-            // Start a timer that calls the resetPagePosition function in 500ms
-            doneResizing = setTimeout(function () {
-                resetPagePosition();
-            }, 500);
-        });
-
-        // Handle mouseover and mouseout events on polaroids
-        document.querySelectorAll('.polaroid').forEach(function (polaroid, index) {
-            polaroid.addEventListener('mouseover', function () {
-                moveImageToSection(index + 1);
-            });
-
-            polaroid.addEventListener('mouseout', function () {
-                resetImage();
-            });
-        });
-
-        function resetPagePosition() {
-            // Clear out the postTop values
-            postTops = [];
-
-            // Push the new top values for each post in the Posts Array
-            posts.forEach(function (post) {
-                postTops.push(Math.floor(post.getBoundingClientRect().top) + window.pageYOffset);
-            });
-
-            // Hold the position of the document
-            const pagePosition = window.scrollY + 300;
-            counter = 0;
-
-            // Increment counter based on the pagePosition 
-            postTops.forEach(function (post) {
-                if (pagePosition > post) {
-                    counter++;
-                }
-            });
-
-            // Ensure counter does not go beyond the length of captions
-            counter = Math.min(counter, captions.length - 1);
-        }
-
-        function moveImageToSection(sectionIndex) {
-            const targetSection = document.querySelector('.sect' + sectionIndex);
-
-            if (targetSection) {
-                const rect = targetSection.getBoundingClientRect();
-                const left = rect.left + window.pageXOffset;
-                const top = rect.top + window.pageYOffset;
-                const scale = 1.2;
-
-                img.style.transition = 'transform 0.5s';
-                img.style.transform = `translate(${left}px, ${top}px) scale(${scale})`;
-
-                // Optionally, you can update the caption accordingly
-                figCaption.innerHTML = captions[sectionIndex];
-            }
-        }
-
-        function resetImage() {
+    function resetImage() {
+        if (!isHovered) {
             img.style.cssText = defaultStyle;
-            figCaption.innerHTML = captions[counter];
+            moveImageToSection(0);
         }
+    }
 
-    }); // end window load function
+    // Initialize to default position
+    moveImageToSection(0);
 
-})(); // END IIFE
+    // Function to manually set translation for each section
+    function getTranslationForSection(sectionIndex) {
+        switch (sectionIndex) {
+            case 1: //yeosang
+                return { left: -800, top: -900, scale: 1.1 };
+            case 2: //hongjoong
+                return { left: 45, top: -450, scale: 1.1 };
+            case 3: //seonghwa
+                return { left: -600, top: -500, scale: 1.2 };
+            case 4: //yunho
+                return { left: -150, top: 125, scale: 1.5 };
+            case 5: //sannie
+                return { left: -550, top: 125, scale: 1.6 };
+            case 6: //mingi
+                return { left: -1100, top: 125, scale: 1.4 };
+            case 7: //wooyoung
+                return { left: -2000, top: -550, scale: 1.2 };
+            case 8: //jongho
+                return { left: -1300, top: -600, scale: 1.2 };
+            default:
+                return { left: -1100, top: -500, scale: 0.3 }; // Default case
+        }
+    }
+
+})();
